222b6fbcc281189141394080ab84bb343a89b632
Student <carla.granof@gmail.com>
2015-02-04 17:30:57 -0700

    added jquery

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
 $(document).on('ready', function() {
+	
+
+
+  $('.submitButton').on('click', function(e){
+    var submittedQuote = $('#quote-edit').val();
+    var submittedAuthor = $('#author-edit').val();
+    console.log(submittedQuote);
+    console.log(submittedAuthor);
+		e.preventDefault();
+    $('#quote-edit').val(' ');
+    $('#author-edit').val(' ');
+
+
+
+  var postedQuote = $('<div class="postedQuote">');
+  var postedAuthor = $('<div class="postedAuthor">');
+  var postedContainer = $('<div class="postedContainer">');
+
+
+  postedContainer
+            .append((postedQuote).text(submittedQuote))
+            .append((postedAuthor).text(submittedAuthor));
+            
+
+
+  $('#posted').append(postedContainer);
+
+  var newPost = postedContainer.clone();
+
+
+
+
+
+
+	})
+
+
   
 });

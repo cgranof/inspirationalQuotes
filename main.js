8fe5cfdef30dce3d1f0d832e829a783a6d8160d8
Student <carla.granof@gmail.com>
2015-02-06 14:03:53 -0700

    styled

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-//Quote constructor 
+///////////////////////
+//Quote constructor //
+/////////////////////
 
 var Quote = function(quotetext, authortext){
   this.quotetext = quotetext
@@ -12,40 +14,42 @@ var Quote = function(quotetext, authortext){
   this.newStar = [star.clone(), star.clone(), star.clone(), star.clone(), star.clone()];
 }
 
+
+//Method to render the quote, author and stars to the quote container
 Quote.prototype.render = function(){
 
   
   this.$el = $('<div class="postedContainer">').clone()
 
-
   
-  this.$el.append('<div class="postedQuote">' +  this.quotetext + '</div>');
-  //this.$el.append('</p>');
+  this.$el.append('<div class="postedQuote">' + this.quotetext + '</div>');
   this.$el.append('<div class="postedAuthor">' + this.author + '</div>');
-  //this.$el.append('</p>');
   this.$el.append(this.newStar);
 
   this.$el.find('.glyphicon-star')
   .on('click', this._starClicked.bind(this));
 
 
-
   return this.$el;
 }
 
+
+//Method to rate the stars, change color on click
 Quote.prototype._starClicked = function(e){
   var currentQuote = this;
   var ratingStar = $(e.currentTarget);
   var rating = ratingStar.index() - 1;
   currentQuote.rating = rating;
 
+
   $(e.currentTarget).siblings().css("color", "grey");
-  $(e.currentTarget).css("color", "yellow");
-  $(e.currentTarget).prevAll().css("color", "yellow");
+  $(e.currentTarget).css("color", "#191919");
+  $(e.currentTarget).prevAll().css("color", "#191919");
 
 }
-
-// Library constructor
+//////////////////////////
+// Library constructor //
+////////////////////////
 
 var Library = function(){
   this.quotes = [];
@@ -53,15 +57,15 @@ var Library = function(){
 }
 
 
-//Push new quotes to the array
+//Method to push new quotes to the array
 Library.prototype.addQuotes = function(quoteArg){
   this.numberOfQuotes = this.quotes.push(quoteArg);
 }
 
+
+//Sort method to re-sort by rating each time the submit button is clicked
+
 Library.prototype.sortByRating = function(){
-
-
-//Bubble Sort to re-sort by rating each time a new quote is rated
 
   var swapQuote;
 
@@ -100,32 +104,25 @@ $(document).on('ready', function() {
 
 
 var myLibrary = new Library();
+
+
 //Click handler for submit form
 
   $('.submitButton').on('click', function(e){
 
     var myQuote = new Quote($('#quote-edit').val(), $('#author-edit').val() );
 
-    
-
-		e.preventDefault();
+    e.preventDefault();
     $('#quote-edit').val(' ');
     $('#author-edit').val(' ');
     
     
 
-    
-
-
     myLibrary.addQuotes(myQuote);
     
     myLibrary.sortByRating();
 
-    
-
     myLibrary.render();
-
-
 
    
   });  
